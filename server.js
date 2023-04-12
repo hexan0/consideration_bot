53328b073a2f6acc494fdd19d0d6f4240b1b23e0
hexan0 <90049658+hexan0@users.noreply.github.com>
2023-04-13 03:00:25 +0900

    GASからのメッセージ受け取り、Discordへの送信

--- server.js
+++ server.js
@@ -17,8 +17,15 @@ http.createServer(function(req, res){
      }
      var dataObject = querystring.parse(data);
      console.log("post:" + dataObject.type);
+    //起動維持のためのpostであるとき
      if(dataObject.type == "wake"){
+      //glitchのログに表示
        console.log("Woke up in post");
+       res.end();
+       return;
+     }
+     if(dataObject.type == "text"){
+      client.channels.get("1073897831837995060").sendMessage(dataObject.text);
        res.end();
        return;
      }
@@ -31,24 +38,38 @@ http.createServer(function(req, res){
  }
 }).listen(3000);
 
+//起動時、コンソールにログ表示
 client.on('ready', message =>{
  console.log('Bot準備完了！');
- client.user.setPresence({ activity: { name: '検討' } });
+ //client.user.setPresence({ activity: { name: '検討' } });
+ client.user.setActivity('検討', { type: 'PLAYING' })
 });
 
+//メッセージ受け取り時？
 client.on('message', message =>{
+ //自身のメッセージには反応しない
  if (message.author.id == client.user.id){
    return;
  }
+ //botに対してのメンションが含まれていたら
  if(message.isMemberMentioned(client.user)){
    sendReply(message, "呼びましたか？");
+  sendMsg(message.channel.id, "てすと");
    return;
  }
- /*if (message.content.match(/にゃ～ん|にゃーん/)){
+//送られたメッセージが /helloだったら
+ if(message.content == "hello"){
+   message.channel.send("HELLO!");
+   let text = "HELLO!";
+   sendMsg(message.channel.id, text);
+        //メッセージが送られたチャンネルに HELLO!と送信する
+   return;
+ }
+ if (message.content.match(/にゃ～ん|にゃーん/)){
    let text = "にゃ～ん";
    sendMsg(message.channel.id, text);
    return;
- }*/
+ }
 });
 
 if(process.env.DISCORD_BOT_TOKEN == undefined){
@@ -58,12 +79,14 @@ process.exit(0);
 
 client.login( process.env.DISCORD_BOT_TOKEN );
 
+//リプライ"＠相手の名前, text"の送信
 function sendReply(message, text){
  message.reply(text)
    .then(console.log("リプライ送信: " + text))
    .catch(console.error);
 }
 
+//メッセージ"text"の送信
 function sendMsg(channelId, text, option={}){
  client.channels.get(channelId).send(text, option)
    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
