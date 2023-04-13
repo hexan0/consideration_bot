bd11d8c96f0f19ea3f1b6ca2e9a45a2252aefccb
hexan0 <90049658+hexan0@users.noreply.github.com>
2023-04-14 02:26:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const http = require('http');
 const querystring = require('querystring-es3');
 const discord = require('discord.js');
 const client = new discord.Client();
+const axiosBase = require("axios"); // どこかに書いておく。
+const url = "/macros/s/AKfycbyxPppdD_JIqMf4jcu2q4cZLYtLuN7Fa38nW5sSMP1BLtKs3IrOIJcHc9cBnZfjsfr3/exec"; // gasのドメイン以降のurl
+//const data = {"key" : "value"}; // 送信するデータ
+//let message = {};
+const { decycle, encycle } = require('json-cyclic');
 
+//GASへの送信
+const axios = axiosBase.create({
+    baseURL: "https://script.google.com", // gas以外の場合はそれぞれ書き換え
+    headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+    },
+    responseType: "json",
+});
+
+//POSTの受信
 http.createServer(function(req, res){
  if (req.method == 'POST'){
    var data = "";
@@ -24,8 +40,10 @@ http.createServer(function(req, res){
        res.end();
        return;
      }
+    //GASからのテキスト送信のためのpostであるとき
      if(dataObject.type == "text"){
-      client.channels.get("1073897831837995060").sendMessage(dataObject.text);
+      //client.channels.get("1073897831837995060").sendMessage(dataObject.text);
+      client.channels.find(ch => ch.name === dataObject.channel_name).sendMessage(dataObject.text);
        res.end();
        return;
      }
@@ -48,7 +66,8 @@ client.on('ready', message =>{
 //メッセージ受け取り時？
 client.on('message', message =>{
  //自身のメッセージには反応しない
- if (message.author.id == client.user.id){
+ //if (message.author.id == client.user.id){
+ if(message.author.bot){
    return;
  }
  //botに対してのメンションが含まれていたら
@@ -58,18 +77,45 @@ client.on('message', message =>{
    return;
  }
 //送られたメッセージが /helloだったら
- if(message.content == "hello"){
+ /*if(message.content == "/hello"){
    message.channel.send("HELLO!");
    let text = "HELLO!";
    sendMsg(message.channel.id, text);
         //メッセージが送られたチャンネルに HELLO!と送信する
    return;
- }
+ }*/
  if (message.content.match(/にゃ～ん|にゃーん/)){
    let text = "にゃ～ん";
    sendMsg(message.channel.id, text);
    return;
  }
+ //それ以外のときはGASにpost
+ if(message.channel.name == "consideration-bot"){
+   console.log(message.content);
+  let decycled_message = decycle(message);
+  decycled_message.content = message.content;
+  axios.post(url, JSON.stringify(decycled_message))
+    .then(async function (response) {
+        const responsedata = response.data; // 受け取ったデータ一覧(object)
+        console.log("post response");
+        console.log(responsedata);
+    })
+    .catch(function (error) {
+        console.log("ERROR!! occurred in Backend.");
+        console.log(error);
+    });
+ }
+ /*axios.post(url, message)
+    .then(async function (response) {
+        const responsedata = response.data; // 受け取ったデータ一覧(object)
+        console.log("post response");
+        console.log(responsedata);
+    })
+    .catch(function (error) {
+        console.log("ERROR!! occurred in Backend.");
+        console.log(error);
+    });*/
+
 });
 
 if(process.env.DISCORD_BOT_TOKEN == undefined){
@@ -92,3 +138,5 @@ function sendMsg(channelId, text, option={}){
    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
    .catch(console.error);
 }
+
+
